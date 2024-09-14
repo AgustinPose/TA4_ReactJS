1790e9d744c5cbc1b7b8a3e7a354c1cbea9097ab
AgustinPose <poseagus15@gmail.com>
2024-09-14 15:34:19 -0300

    TA4

---
+++ ta4/src/contador.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './App.css'; 
+
+function Contador() {
+  const [count, setCount] = useState(0);
+
+
+  const incrementar = () => setCount(count + 1);
+
+  const decrementar = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const reiniciar = () => setCount(0);
+
+  return (
+    <div className="counter-container">
+      <h1>Contador: {count === 9 ? 'Luis Suarez' : count}</h1>
+      
+      <div className="buttons">
+        <button className="button is-primary" onClick={incrementar}>Incrementar</button>
+        <button className="button is-warning" onClick={decrementar}>Decrementar</button>
+        <button className="button is-danger" onClick={reiniciar}>Reiniciar</button>
+      </div>
+      <br></br>
+      {count === 9 && (
+        <div>
+          <img 
+            src="https://monesport.cat/app/uploads/sites/12/2022/12/europapress_4850452_02_december_2022_qatar_al_wakrah_uruguays_luis_suarez_celebrates_after_his-1-scaled.jpg" 
+            alt="Luis Suárez" 
+            style={{ width: '200px', borderRadius: '10px' }}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Contador;
